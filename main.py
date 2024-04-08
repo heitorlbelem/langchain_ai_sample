e38c801fe006a80c424b60bda9d86087d193eda3
Heitor Belem <heitorlbelem98@gmail.com>
2024-04-08 19:59:55 -0300

    adds a context for generate response

--- main.py
+++ main.py
@@ -3,18 +3,38 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_community.document_loaders.web_base import WebBaseLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.documents import Document
 
 load_dotenv()
 
 llm = ChatOpenAI()
 
-prompt = ChatPromptTemplate.from_messages([
-  ("system", "You are world class technical documentation writer."),
-  ("user", "{input}")
-])
+loader = WebBaseLoader("https://docs.smith.langchain.com/user_guide")
+docs = loader.load()
+embeddings = OpenAIEmbeddings()
 
-chain = prompt | llm | StrOutputParser()
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+vector = FAISS.from_documents(documents, embeddings)
 
-response = chain.invoke({ "input": "do I have to test my application before deploy?" })
+
+prompt = ChatPromptTemplate.from_template(
+  """
+    Answer the following question based only on the provided context:
+    <context> {context} </context>
+    Question: {input}
+  """
+)
+document_chain = create_stuff_documents_chain(llm, prompt)
+
+response = document_chain.invoke({
+    "input": "how can langsmith help with testing?",
+    "context": [Document(page_content="langsmith é ruim pra testar")]
+})
 
 print(response)
